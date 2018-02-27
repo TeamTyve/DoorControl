16122fe92d4466d6e130def34d11215989a9a2fe
StefanieNielson <nielson1996@hotmail.com>
2018-02-27 09:15:52 +0100

    Added test

--- src/DoorControl/DoorControl.Unit.Test/DoorControlTest.cs
+++ src/DoorControl/DoorControl.Unit.Test/DoorControlTest.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using DoorControl;
 
 namespace DoorControl.Unit.Test
 {
     public class DoorControlTest
     {
+        private Program uut = new Program();
+        [Test]
+        public void AddTest()
+        {
+            Assert.That(uut.Add(2,2), Is.EqualTo(4));
+        }
+
     }
 }
--- src/DoorControl/DoorControl/Program.cs
+++ src/DoorControl/DoorControl/Program.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace DoorControl
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+        }
 
+        public int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
